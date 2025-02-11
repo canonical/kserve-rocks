91153a6f9a0f0f472c16823002cffd8d197161cc
NohaIhab <noha.ihab@canonical.com>
2025-02-11 14:27:52 +0200

    update sanity tests with python 3.11

--- storage-initializer/tests/test_rock.py
+++ storage-initializer/tests/test_rock.py
@@ -42,7 +42,7 @@ def test_rock(rock_test_env):
             "/bin/bash",
             LOCAL_ROCK_IMAGE,
             "-c",
-            "ls -la /usr/local/lib/python3.10/dist-packages/kserve",
+            "ls -la /usr/local/lib/python3.11/dist-packages/kserve",
         ],
         check=True,
     )
