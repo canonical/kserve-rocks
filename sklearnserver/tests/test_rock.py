0a11438f803c0c1f94a787e9e5ca4c589003f3ee
MattiaSarti <nmattia1@gmail.com>
2026-04-08 12:13:29 +0200

    chore: upgrade sklearnserver to 0.17.0

--- sklearnserver/tests/test_rock.py
+++ sklearnserver/tests/test_rock.py
@@ -59,6 +59,18 @@ def test_rock(rock_test_env):
             "/bin/bash",
             LOCAL_ROCK_IMAGE,
             "-c",
+            "ls -la /usr/local/lib/python3.11/dist-packages/storage",
+        ],
+        check=True,
+    )
+    subprocess.run(
+        [
+            "docker",
+            "run",
+            "--entrypoint",
+            "/bin/bash",
+            LOCAL_ROCK_IMAGE,
+            "-c",
             "ls -la /usr/local/lib/python3.11/dist-packages/kserve",
         ],
         check=True,
